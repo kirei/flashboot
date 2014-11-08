10259cf55998a3b5b56c9a310fc5dc7b45f5d05f
Bastien Durel <bastien@durel.org>
2014-11-08 08:21:20 +0100

    modifications for 5.6

--- build-release.sh
+++ build-release.sh
@@ -6,8 +6,8 @@ WORKDIR=sandbox
 export CWD WORKDIR
 
 # Update for each new release
-SHORTREL="55"
-LONGREL="5.5"
+SHORTREL="56"
+LONGREL="5.6"
 
 # No need to change anything below this line for new OS releases!
 
